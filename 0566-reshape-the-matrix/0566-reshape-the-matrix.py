28990596e727d7498b3b7cec78608f81b99ad7f9
Shivam <45448915+shivamkchoudhary@users.noreply.github.com>
2023-01-10 12:55:45 +0530

    Time: 154 ms (49.20%), Space: 32.8 MB (5.56%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,9 @@
+import numpy as np
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        
+        try:
+            return np.reshape(mat, (r,c)).tolist()
+        except:
+            return mat
+        
