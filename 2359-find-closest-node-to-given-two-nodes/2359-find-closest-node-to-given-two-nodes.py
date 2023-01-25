0ae9c4de5eaf358552b31ca06deddb9ee7b223e6
Shivam <45448915+shivamkchoudhary@users.noreply.github.com>
2023-01-25 23:06:28 +0530

    Time: 2364 ms (27.59%), Space: 58.4 MB (37.93%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -7,21 +7,18 @@ class Solution:
         def bfs(src, distMap):
             q = deque()
             q.append([src, 0])
-            distMap[src] = 0
-            
+            distMap[src] = 0            
             while q:
                 node, dist = q.popleft()
                 for neg in adj[node]:
                     if neg not in distMap:
                         q.append([neg, dist + 1])
-                        distMap[neg] = dist + 1
-                    
+                        distMap[neg] = dist + 1                    
         node1Dict = {}
         node2Dict = {}
         
         bfs(node1, node1Dict)
-        bfs(node2, node2Dict)
-        
+        bfs(node2, node2Dict)        
         output = -1
         tempDist = float("inf")
         for i in range(len(edges)):
