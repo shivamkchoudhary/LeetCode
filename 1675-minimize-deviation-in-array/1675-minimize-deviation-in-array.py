b15977880fb44f12d3726e7ac51da3d3f60cae10
Shivam <45448915+shivamkchoudhary@users.noreply.github.com>
2023-02-24 22:05:33 +0530

    Time: 3221 ms (28.36%), Space: 23.6 MB (26.87%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumDeviation(self, nums: List[int]) -> int:
+        minHeap, heapMax = [], 0
+        for n in nums:
+            temp = n
+            while n% 2 == 0:
+                n = n // 2            
+            minHeap.append((n, max(temp, 2 * n)))
+            heapMax = max(heapMax, n)
+            
+        res = float("inf")
+        heapq.heapify(minHeap)
+        while len(minHeap) == len(nums):
+            n, nMax = heapq.heappop(minHeap)
+            res = min(res, heapMax - n)
+            
+            if n < nMax:
+                heapq.heappush(minHeap, (n * 2, nMax))
+                heapMax = max(heapMax, n * 2)
+        return res
+        
+        
