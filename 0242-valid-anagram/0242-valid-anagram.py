68ee42b9f8d8381f23edc59cc03b7099e5b8e77d
Shivam <45448915+shivamkchoudhary@users.noreply.github.com>
2023-03-19 21:17:51 +0530

    Time: 28 ms (99.61%), Space: 14.5 MB (60.64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,9 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        # return all(s.count(x) == t.count(x) for x in set(s+t))
-        s1=sorted(s)
-        s2=sorted(t)
-        if s1==s2:
-            return True
-        return False
+        return all(s.count(x) == t.count(x) for x in set(s+t))
+        # s1=sorted(s)
+        # s2=sorted(t)
+        # if s1==s2:
+        #     return True
+        # return False
