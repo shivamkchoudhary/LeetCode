ef56f344ed808338f03dc5ee52711e41a3bff10b
Shivam <45448915+shivamkchoudhary@users.noreply.github.com>
2023-03-22 14:34:47 +0530

    Time: 233 ms (76.54%), Space: 21.2 MB (67.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,7 +6,7 @@ class Solution:
         right_product = 1
 
         for i in range(n):
-            output[i] = output[i] * left_product
+            output[i] = left_product
             left_product = left_product * nums[i]
 
         for i in range(n-1, -1, -1):
