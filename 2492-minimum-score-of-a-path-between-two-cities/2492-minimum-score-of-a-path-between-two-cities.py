9b19d7b951deab24f06c29975a78dab5a2c76241
Shivam <45448915+shivamkchoudhary@users.noreply.github.com>
2023-03-22 16:37:20 +0530

    Time: 1791 ms (65.77%), Space: 74.2 MB (41.61%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        adj = defaultdict(list)
+        for src, dst, dist in roads:
+            adj[src].append((dst, dist))
+            adj[dst].append((src, dist))
+            
+        def dfs(i):
+            if i in visit:
+                return
+            visit.add(i)
+            nonlocal res
+            for nei, dist in adj[i]:
+                res = min(res, dist)
+                dfs(nei)                    
+        res = float("inf")
+        visit = set()
+        dfs(1)
+        return res
+        
